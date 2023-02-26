1e0ba1327313e2b475ccb0914463c0e319997f37
varsubham <var.subham@gmail.com>
2023-02-27 00:12:16 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: "hello world"
+    })
+})
+
+app.listen(8080, () => {
+    console.log('server running on port 8080')
+})
